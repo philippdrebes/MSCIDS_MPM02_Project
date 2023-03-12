c4c040dd7d5a0fee3621e77a87eaca91f16b2010
Philipp Drebes <philipp.drebes@gmail.com>
2023-03-12 16:52:50 +0100

    data preparation and one-hot encoding

--- setup.r
+++ setup.r
@@ -1,11 +1,6 @@
-install.packages(c("fpp2", "ggplot2", "Mcomp", "tidyverse", "kernlab", "ISLR", "e1071", "RColorBrewer"), quiet=TRUE);
-suppressPackageStartupMessages(library("fpp2"))
-library(fpp2, quietly = T);
-library(ggplot2, quietly = T);
-library(Mcomp, quietly = T);
-library(tidyverse, quietly = T);
-library(kernlab, quietly = T);
-library(ISLR, quietly = T);
-library(e1071, quietly = T);
-library(RColorBrewer, quietly = T);
-#options(repr.plot.width=5, repr.plot.height=4);
+install.packages(c("caret", "vtreat", "corrplot"), quiet=TRUE)
+# library(tidyverse, quietly = T)
+library(caret, quietly = T)
+library(vtreat, quietly = T)
+library(corrplot, quietly = T)
+library(readr,  quietly = T)
